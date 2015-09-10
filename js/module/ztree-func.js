a8a9de1cd6d97e35fbd773f709c7bf48957372ec
IChocolateKapa <echochocolatekapa@163.com>
2015-09-10 13:33:31 +0800

    节点排序 增加无子节点时节点图标

--- js/module/ztree-func.js
+++ js/module/ztree-func.js
@@ -273,7 +273,7 @@ var setting = {
 };
 
 var zNodes =[
-    { id:1, pId:0, name:"我的程序", open:true},
+    { id:1, pId:0, name:"我的程序", open:true, iconOpen:"images/yiji.png", iconClose:"images/yijiclose.png"},
     { id:11, pId:1, name:"常用程序"},
     { id:12, pId:1, name:"不用程序"},
     { id:121, pId:12, name:"YYY用程序"},
@@ -283,19 +283,10 @@ var zNodes =[
     { id:112, pId:11, name:"Executable file"},
     { id:113, pId:11, name:"Portable object files"},
     { id:113, pId:11, name:"Aortable object files"},
-    { id:2, pId:0, name:"王小明01的程序"},
-    { id:21, pId:2, name:"常用程序"},
-    { id:211, pId:21, name:"Portable files"},
-    { id:22, pId:2, name:"不常用程序"},
-    { id:3, pId:0, name:"张明04的程序"},
-    { id:31, pId:3, name:"Documents"},
-    { id:32, pId:3, name:"Photos"},
-    { id:4, pId:0, name:"李明明的程序"},
-    { id:41, pId:4, name:"Documents"},
-    { id:42, pId:4, name:"Photos"},
-    { id:5, pId:0, name:"赵大明01的程序"},
-    { id:51, pId:5, name:"Documents"},
-    { id:52, pId:5, name:"Photos"}
+    { id:2, pId:0, name:"王小明01的程序", iconOpen:"images/yiji.png", iconClose:"images/yijiclose.png"},
+    { id:3, pId:0, name:"张明04的程序", iconOpen:"images/yiji.png", iconClose:"images/yijiclose.png"},
+    { id:4, pId:0, name:"李明明的程序", iconOpen:"images/yiji.png", iconClose:"images/yijiclose.png"},
+    { id:5, pId:0, name:"赵大明01的程序", iconOpen:"images/yiji.png", iconClose:"images/yijiclose.png"}
 ];
 
 function focusOn(treeId, treeNode){
@@ -362,79 +353,55 @@ $(function(){
     setSortIcon();
 
     /*点击排序时*/
+    //$(".ztree, .ztree").on("click", "li.level0>a .btn-sort, li.level0>a  .btn-sort", function(){
     $(".ztree li.level0>a, .ztree li.level1>a").on("click", ".btn-sort", function(){
         var $this = $(this);
+        var closetLi = $this.closest("li");
+        var curUL = closetLi.children("ul");
+        var curLi = curUL.children("li");
+
 
         var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-        var curTid = $this.closest("li").attr("id");
-        var curNode = zTree.getNodeByParam("tId", curTid);
 
-        var curNodeChildren = curNode.children;
-        var lenChildren = curNodeChildren.length;
+
+        var data = {};
+        var dataArr = [];
+        curLi.each(function(index, ele){
+            var id = $(ele).attr("id");
+            var curNode = zTree.getNodeByParam("tId", id);
+            data[id] = ele;
+            dataArr.push(curNode);
+        });
+
+        /*这个是对汉字拼音的排序， 返回的是排序好的子节点数组*/
+        var dataNew = dataArr.sort(function(a,b){return a.name.localeCompare(b.name)});
+        $(curUL).empty();
 
         /*图标为升序，进行升序排列*/
         if($this.hasClass("sheng")){
             /*升序排列*/
             /*子节点数目大于1时才进行排序*/
+            $this.removeClass("sheng");
+            $this.addClass("jiang");
 
-            if(lenChildren > 1){
-                /*这个是对汉字拼音的排序， 返回的是排序好的子节点数组*/
-                curNodeChildren = curNodeChildren.sort(function(a,b){return a.name.localeCompare(b.name)});
-                /*这里是最关键的，数组克隆*/
-                /*如果不克隆的话，每当remove掉一个，原来的数组就少一个值，循环会中断*/
-                curNodeChildrenBak = curNodeChildren.slice(0);
-                for(var i = 0; i < lenChildren; i++){
-
-                    var curSelNode = zTree.getNodeByParam("id", curNodeChildrenBak[i].id);
-
-                    zTree.removeNode(curSelNode);
-
-                    console.log("curSelNode", i, ": ", curSelNode);
-
-                    var newNode = zTree.addNodes(curNode, curSelNode);
-                    console.log("newNode: ", newNode);
-                    //$(".ztree li.level0>a, .ztree li.level1>a").append("<i class='btn-sort sheng'>");
-
-
-                    $this.removeClass("sheng");
-                    $this.addClass("jiang");
-                }
+            for(var i = 0; i < dataNew.length; i++){
+                var idTar = dataNew[i].tId;
+                $(curUL).append(data[idTar]);
             }
-
-            curNodeChildren = curNode.children;
-
-
         } else {
-
-            /*降序排列*/
-            /*子节点数目大于1时才进行排序*/
-            if(curNodeChildren.length > 1){
-                /*这个是对汉字拼音的排序， 返回的是排序好的子节点数组*/
-                curNodeChildren = curNodeChildren.sort(function(a,b){return a.name.localeCompare(b.name)});
-
-                /*这里是最关键的，数组克隆*/
-                /*如果不克隆的话，每当remove掉一个，原来的数组就少一个值，循环会中断*/
-                curNodeChildrenBak = curNodeChildren.slice(0);
-                console.log("降序时， curNodeChildrenBak： ", curNodeChildrenBak);
-                for(var i = lenChildren-1; i > -1; i--){
-
-                    var curSelNode = zTree.getNodeByParam("id", curNodeChildrenBak[i].id);
-
-                    zTree.removeNode(curSelNode);
-
-                    console.log("curSelNode", i, ": ", curSelNode);
-
-                    zTree.addNodes(curNode, curSelNode);
-                    //$(".ztree li.level0>a, .ztree li.level1>a").append("<i class='btn-sort sheng'>");
-                }
-            }
-
-
             /*图标为降序， 进行降序排列*/
             $this.addClass("sheng");
             $this.removeClass("jiang");
+            /*降序排列*/
+            /*子节点数目大于1时才进行排序*/
+            for(var i = 0; i < dataNew.length; i++){
+                var idTar = dataNew[i].tId;
+                $(curUL).prepend(data[idTar]);
+            }
 
         }
+        eventUtil.preventDefault(event);
+        eventUtil.stopPropagation(event);
     })
 
 });
